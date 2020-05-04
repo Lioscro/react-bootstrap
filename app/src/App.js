5cd47a638b562c0ba1e15fc53a572698ba5d8c1c
Lioscro <kmin@caltech.edu>
2020-05-04 12:31:27 -0700

    initial commit

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Container>
+        App
+      </Container>
+    );
+  }
+}
+export default App;
